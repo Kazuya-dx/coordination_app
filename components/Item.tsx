523350c821112c4a92eabaf2933b7691317adb67
BuildTools <kachiiiscool@gmail.com>
2020-11-12 19:30:29 +0900

    coordination commit

--- components/Item.tsx
+++ components/Item.tsx
@@ -1,30 +1,63 @@
-import React, {useState} from "react";
+import React, { useEffect, useState } from "react";
 import { View, Image } from "react-native";
 
-const Item: React.FC = ({imageUrl}) => {
-    const [pos, setPos] = useState({x: 0, y: 0})
-    const [lastPress, setLastPress] = useState(0)
-    return <View 
-        onStartShouldSetResponder={e => true}
-        onResponderMove={(e) => {
-        let x = e.nativeEvent.pageY - 360
-        let y = e.nativeEvent.pageX - 100
-        setPos({x: x, y: y})
-        }}
-        onTouchStart={() => {
+type Props = {
+  source: any;
+  width: number;
+};
+
+const Item: React.FC<Props> = (props) => {
+  const [onMove, setOnMove] = useState(false);
+  const [border, setBorder] = useState(0);
+  const [pos, setPos] = useState({ x: 0, y: 0 });
+  const [lastPress, setLastPress] = useState(0);
+
+  useEffect(() => {
+    if (onMove) {
+      setBorder(1);
+    } else {
+      setBorder(0);
+    }
+  });
+
+  return (
+    <View
+      onStartShouldSetResponder={(e) => true}
+      onResponderMove={(e) => {
+        let x = e.nativeEvent.pageY - 280;
+        let y = e.nativeEvent.pageX - 120;
+        setPos({ x: x, y: y });
+      }}
+      onTouchStart={() => {
         // ダブルタップ処理
         const now = new Date().getTime();
-        if ((now - lastPress) <= 250) {
-            console.log("DOUBLE TAP")
-        } 
-        setLastPress(now)
-        }}
+        if (now - lastPress <= 250) {
+          console.log("DOUBLE TAP");
+        }
+        setLastPress(now);
+
+        // onMoveの処理
+        setOnMove(true);
+      }}
+      onTouchEnd={() => {
+        setOnMove(false);
+      }}
     >
-        <Image 
-        style={{ position: "absolute", top: pos.x, left: pos.y, width: 190, resizeMode: "contain"}}
-        source={require(imageUrl)}
-        />
+      <Image
+        style={{
+          position: "absolute",
+          top: pos.x,
+          left: pos.y,
+          height: props.width,
+          width: props.width,
+          resizeMode: "contain",
+          borderWidth: border,
+          borderColor: "#a1b",
+        }}
+        source={props.source}
+      />
     </View>
-}
+  );
+};
 
 export default Item;
--- screens/CoordinationScreen.tsx
+++ screens/CoordinationScreen.tsx
@@ -1,37 +1,126 @@
-import React, {useState} from "react";
-import { View, ImageBackground, Image} from "react-native";
+import React, { useState } from "react";
+import { View, Text, ImageBackground, ViewStyle } from "react-native";
+import Item from "../components/Item";
 
 const CoordinationScreen: React.FC = () => {
-  const [pos, setPos] = useState({x: 0, y: 0})
-  const [lastPress, setLastPress] = useState(0)
+  const [tops, setTops] = useState("");
+  const [pants, setPants] = useState("");
+  const [tuckIn, setTuckIn] = useState({
+    check: false,
+    display: "OFF",
+    zIndex: 0,
+  });
+
   return (
-      <ImageBackground 
-        source={require("../assets/test_model.png")} 
-        style={{ flex: 1 }}>
-        <View 
-          onStartShouldSetResponder={e => true}
-          onResponderMove={(e) => {
-            let x = e.nativeEvent.pageY - 360
-            let y = e.nativeEvent.pageX - 100
-            setPos({x: x, y: y})
+    <View style={{ flex: 1 }}>
+      <View
+        style={{
+          flex: 1,
+          backgroundColor: "#fff",
+          flexDirection: "row",
+          alignItems: "center",
+          zIndex: 3,
+        }}
+      >
+        <ImageBackground
+          source={require("../assets/test_tops.png")}
+          style={menuStyle}
+        >
+          <View
+            style={{
+              backgroundColor: "#FFF",
+              width: "100%",
+              justifyContent: "center",
+              alignItems: "center",
+              opacity: 0.7,
+            }}
+          >
+            <Text style={{ fontSize: 12, fontWeight: "bold" }}>Tops</Text>
+          </View>
+        </ImageBackground>
+        <ImageBackground
+          source={require("../assets/test_pants.png")}
+          style={menuStyle}
+        >
+          <View
+            style={{
+              backgroundColor: "#FFF",
+              width: "100%",
+              justifyContent: "center",
+              alignItems: "center",
+              opacity: 0.7,
+            }}
+          >
+            <Text style={{ fontSize: 12, fontWeight: "bold" }}>Pants</Text>
+          </View>
+        </ImageBackground>
+        <View
+          style={{
+            justifyContent: "flex-end",
+            alignItems: "center",
+            width: 60,
+            height: 60,
+            padding: 3,
+            margin: 15,
+            backgroundColor: "#ddd",
           }}
           onTouchStart={() => {
-            // ダブルタップ処理
-            const now = new Date().getTime();
-            if ((now - lastPress) <= 250) {
-              console.log("DOUBLE TAP")
-            } 
-            setLastPress(now)
+            if (tuckIn.check) {
+              setTuckIn({ check: false, display: "OFF", zIndex: 0 });
+            } else {
+              setTuckIn({ check: true, display: "ON", zIndex: 2 });
+            }
           }}
         >
-          <Image 
-            style={{ position: "absolute", top: pos.x, left: pos.y, width: 190, resizeMode: "contain"}}
-            source={require("../assets/test_tops.png")}
-          />
+          <View
+            style={{
+              justifyContent: "center",
+              alignItems: "center",
+              width: "100%",
+              height: "80%",
+            }}
+          >
+            <Text style={{ fontSize: 20, color: "green" }}>
+              {tuckIn.display}
+            </Text>
+          </View>
+
+          <View
+            style={{
+              backgroundColor: "#FFF",
+              width: "100%",
+              justifyContent: "center",
+              alignItems: "center",
+              opacity: 0.7,
+            }}
+          >
+            <Text style={{ fontSize: 12, fontWeight: "bold" }}>Tuck In</Text>
+          </View>
         </View>
-       
+      </View>
+      <ImageBackground
+        source={require("../assets/test_model.png")}
+        style={{ flex: 9, zIndex: 0 }}
+      >
+        <View style={{ zIndex: tuckIn.zIndex }}>
+          <Item source={require("../assets/test_pants.png")} width={270} />
+        </View>
+        <View style={{ zIndex: 1 }}>
+          <Item source={require("../assets/test_tops.png")} width={220} />
+        </View>
       </ImageBackground>
+    </View>
   );
+};
+
+const menuStyle: ViewStyle = {
+  justifyContent: "flex-end",
+  alignItems: "center",
+  width: 60,
+  height: 60,
+  padding: 3,
+  margin: 15,
+  backgroundColor: "#ddd",
 };
 
 export default CoordinationScreen;
